522ef62dd68e6ff03f6a7f415cd837bc375faa0f
MekronOne <xmark878@gmail.com>
2026-05-18 00:58:27 +0300

    i love asm!!!

--- hello.asm
+++ hello.asm
@@ -1,1 +1,17 @@
-something happen?
+section .data
+    msg db "Hello, Friend!", 10
+    msg_len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, msg_len
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
